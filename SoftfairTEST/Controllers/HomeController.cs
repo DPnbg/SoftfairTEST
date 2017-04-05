9f2cb7383e4e43e6a8ccd7422fb7b011eece4ff8
Dominik Pfefferle <Dominik.Pfefferle@nuernberger.de>
2017-04-05 08:43:14 +0200

    2tes PDF

--- SoftfairTEST/Controllers/HomeController.cs
+++ SoftfairTEST/Controllers/HomeController.cs
@@ -9,54 +9,70 @@ namespace SoftfairTEST.Controllers {
 		public ActionResult Index() {
 
 
-			var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("SoftfairTEST.SLS_NES2800P_a.pdf");
-			//PDF auspacken
-			byte[] BT4allFile = null;
-			iTextSharp.text.pdf.PdfReader reader;
-			try {
-				reader = new iTextSharp.text.pdf.PdfReader(stream);
-				iTextSharp.text.pdf.PdfDictionary root = reader.Catalog;
-				iTextSharp.text.pdf.PdfDictionary names = root.GetAsDict(iTextSharp.text.pdf.PdfName.NAMES);
-				if (names != null) {
-					iTextSharp.text.pdf.PdfDictionary embeddedFiles = names.GetAsDict(iTextSharp.text.pdf.PdfName.EMBEDDEDFILES);
-					if (embeddedFiles != null) {
 
-						var en = embeddedFiles.Keys.GetEnumerator();
-						while (en.MoveNext()) {
-							var obj = embeddedFiles.GetAsArray(en.Current as iTextSharp.text.pdf.PdfName);
+			return View();
+		}
 
-							iTextSharp.text.pdf.PdfDictionary fileSpec = obj.GetAsDict(1);
+        public ActionResult OpenNBG(string fname)
+        {
+            var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(fname);
+            //PDF auspacken
+            byte[] BT4allFile = null;
+            iTextSharp.text.pdf.PdfReader reader;
+            try
+            {
+                reader = new iTextSharp.text.pdf.PdfReader(stream);
+                iTextSharp.text.pdf.PdfDictionary root = reader.Catalog;
+                iTextSharp.text.pdf.PdfDictionary names = root.GetAsDict(iTextSharp.text.pdf.PdfName.NAMES);
+                if (names != null)
+                {
+                    iTextSharp.text.pdf.PdfDictionary embeddedFiles = names.GetAsDict(iTextSharp.text.pdf.PdfName.EMBEDDEDFILES);
+                    if (embeddedFiles != null)
+                    {
 
-							iTextSharp.text.pdf.PdfDictionary file = fileSpec.GetAsDict(iTextSharp.text.pdf.PdfName.EF);
+                        var en = embeddedFiles.Keys.GetEnumerator();
+                        while (en.MoveNext())
+                        {
+                            var obj = embeddedFiles.GetAsArray(en.Current as iTextSharp.text.pdf.PdfName);
 
-							foreach (iTextSharp.text.pdf.PdfName key in file.Keys) {
-								iTextSharp.text.pdf.PRStream innerstream = (iTextSharp.text.pdf.PRStream)
-									iTextSharp.text.pdf.PdfReader.GetPdfObject(file.GetAsIndirectObject(key));
+                            iTextSharp.text.pdf.PdfDictionary fileSpec = obj.GetAsDict(1);
 
-								if (innerstream != null) {
-									BT4allFile = iTextSharp.text.pdf.PdfReader.GetStreamBytes(innerstream);
-									break;
-								}
-							}
-						}
-					}
-				}
-			}catch(Exception ex) {
+                            iTextSharp.text.pdf.PdfDictionary file = fileSpec.GetAsDict(iTextSharp.text.pdf.PdfName.EF);
 
-			}
+                            foreach (iTextSharp.text.pdf.PdfName key in file.Keys)
+                            {
+                                iTextSharp.text.pdf.PRStream innerstream = (iTextSharp.text.pdf.PRStream)
+                                    iTextSharp.text.pdf.PdfReader.GetPdfObject(file.GetAsIndirectObject(key));
 
-			//Request mit Post des Falls und Response mit der ID
-			string StartID = null;
-			if (BT4allFile != null) {
-				try {
-					var Req = System.Net.WebRequest.Create("http://localhost/BT4All/SV/d.svc/m?i=getStartID_SLS_NoSign");
-					Req.Method = "POST";
+                                if (innerstream != null)
+                                {
+                                    BT4allFile = iTextSharp.text.pdf.PdfReader.GetStreamBytes(innerstream);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            //Request mit Post des Falls und Response mit der ID
+            string StartID = null;
+            if (BT4allFile != null)
+            {
+                try
+                {
+                    var Req = System.Net.WebRequest.Create("http://localhost/BT4All/SV/d.svc/m?i=getStartID_SLS_NoSign");
+                    Req.Method = "POST";
 
                     var xtraData = new byte[100];
                     for (int x = 0; x < xtraData.Length; x++)
                         xtraData[x] = 1;
 
-					Req.ContentLength = BT4allFile.Length+xtraData.Length;
+                    Req.ContentLength = BT4allFile.Length + xtraData.Length;
                     if (xtraData.Length > 0)
                     {
                         Req.Headers.Add("xtraDataLen", xtraData.Length.ToString());
@@ -65,29 +81,32 @@ namespace SoftfairTEST.Controllers {
                     reqS.Write(BT4allFile, 0, BT4allFile.Length);
                     reqS.Write(xtraData, 0, xtraData.Length);
                     var resp = Req.GetResponse();
-					Newtonsoft.Json.JsonTextReader jReader = new Newtonsoft.Json.JsonTextReader(
-						new System.IO.StreamReader(resp.GetResponseStream()));
-					while (jReader.Read()) {
-						var tp = jReader.TokenType;
-						var val = jReader.Value;
-						if(tp == Newtonsoft.Json.JsonToken.PropertyName && (val as string) == "sid") {
-							StartID = jReader.ReadAsString();
-							break;
-						}
-					}
+                    Newtonsoft.Json.JsonTextReader jReader = new Newtonsoft.Json.JsonTextReader(
+                        new System.IO.StreamReader(resp.GetResponseStream()));
+                    while (jReader.Read())
+                    {
+                        var tp = jReader.TokenType;
+                        var val = jReader.Value;
+                        if (tp == Newtonsoft.Json.JsonToken.PropertyName && (val as string) == "sid")
+                        {
+                            StartID = jReader.ReadAsString();
+                            break;
+                        }
+                    }
 
-				}catch(Exception ex) {
+                }
+                catch (Exception ex)
+                {
 
-				}
-			}
-			//=>iframe wird in der Views/Home/INDEX.cshtml aufgebaut
-			this.ViewBag.StartID = StartID;
+                }
+            }
+            //=>iframe wird in der Views/Home/INDEX.cshtml aufgebaut
+            this.ViewBag.StartID = StartID;
+            return View();
+        }
 
 
-			return View();
-		}
-
-		public ActionResult About() {
+        public ActionResult About() {
 			ViewBag.Message = "Your application description page.";
 
 			return View();
